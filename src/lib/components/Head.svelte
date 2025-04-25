3ef06339198d75c60c75ce1ea6315876e16d681d
phenomen <phenomen@pm.me>
2025-04-25 14:57:08 -0500

    Unused files

--- src/lib/components/Head.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	let { meta }: { meta: { title: string; description: string } } = $props();
-</script>
-
-<svelte:head>
-	<title>{meta.title}</title>
-	<meta name="description" content={meta.description} />
-</svelte:head>
